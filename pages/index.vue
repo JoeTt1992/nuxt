105003fe0bb4ac11ffed382a5901fb32b25079e9
Joe <363379185@qq.com>
2018-03-07 10:21:25 +0800

    add form group

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,13 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <el-button type="primary" class="mybtn" @click="selectCity()">选择城市</el-button>
       <el-radio v-model="radio" label="1">北京</el-radio>
       <el-radio v-model="radio" label="2">上海</el-radio>
+    </div>
+    <div>
+      <el-button type="danger" class="mybtn" @click="returnName()">重置姓名</el-button>
+      <el-input v-model="name" placeholder="请输入姓名"></el-input>
     </div>
   </section>
 </template>
@@ -38,8 +26,22 @@ export default {
   },
   data(){
     return{
-      radio : '2'
+      radio : '1',
+      name:''
     };
+  },
+  methods:{
+    selectCity: function(){
+      if(this.radio == '1'){
+        this.radio = '2';
+      }
+      else{
+        this.radio = '1';
+      }
+    },
+    returnName: function(){
+      this.name = ''
+    }
   }
 }
 </script>
@@ -53,24 +55,7 @@ export default {
   text-align: center;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.mybtn{
+  margin-right: 20px;
 }
 </style>
